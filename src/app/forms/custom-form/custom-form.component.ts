ac289c067501387552167e6ce653cd8b6de5d407
Beni312 <bence312@gmail.com>
2018-02-26 17:43:15 +0100

    custom form input

--- src/app/forms/custom-form/custom-form.component.ts
+++ src/app/forms/custom-form/custom-form.component.ts
@@ -1,15 +1,39 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnChanges, SimpleChanges } from '@angular/core';
+import { ControlValueAccessor } from "@angular/forms";
 
 @Component({
   selector: 'app-custom-form',
   templateUrl: './custom-form.component.html',
   styleUrls: ['./custom-form.component.css']
 })
-export class CustomFormComponent implements OnInit {
+export class CustomFormComponent implements ControlValueAccessor, OnChanges {
 
-  constructor() { }
+  text= '';
+  propagateChange: any = () => {};
+  @Input()
+  placeholder: string;
 
-  ngOnInit() {
+  constructor() {
+  }
+
+  clearSearch() {
+    this.text = '';
+  }
+
+  writeValue(obj: any): void {
+  }
+
+  registerOnChange(fn: any): void {
+    this.propagateChange = fn;
+  }
+
+  registerOnTouched(fn: any): void {
+  }
+
+  ngOnChanges(changes: SimpleChanges): void {
+    if (changes['text'] && changes['text'].currentValue) {
+      this.propagateChange(this.text);
+    }
   }
 
 }
--- src/app/forms/forms.example.component.ts
+++ src/app/forms/forms.example.component.ts
@@ -2,6 +2,7 @@ import {Component} from "@angular/core";
 
 @Component({
   selector: 'app-forms-example',
+  styleUrls: ['./forms.example.component.css'],
   templateUrl: './forms.example.component.html'
 })
 export class FormsExampleComponent {
