5dde60bece5eb14863f28b38943d802de8f2f7a4
Beni312 <bence312@gmail.com>
2018-03-01 17:05:06 +0100

    menu, code clean

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,12 +1,12 @@
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from "@angular/platform-browser/animations";
 import { BrowserModule } from '@angular/platform-browser';
+import { CoreModule } from "./core/core.module";
+import { ErrorComponent } from "./core/error/error.component";
+import { HeroesModule } from "./heroes/heroes.module";
 import { ModuleWithProviders, NgModule } from '@angular/core';
 import { RouterModule } from "@angular/router";
 import { ToasterModule } from "angular5-toaster/dist";
-import { ErrorComponent } from "./core/error/error.component";
-import { HeroesModule } from "./heroes/heroes.module";
-import { CoreModule } from "./core/core.module";
 
 const Routing: ModuleWithProviders = RouterModule.forRoot([
   {
