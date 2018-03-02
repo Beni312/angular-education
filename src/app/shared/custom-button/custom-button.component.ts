246d5ce4f8d93875a05817e2518752e4d60a66d1
Beni312 <bence312@gmail.com>
2018-03-02 15:23:48 +0100

    custom button triggered twice fix

--- src/app/shared/custom-button/custom-button.component.ts
+++ src/app/shared/custom-button/custom-button.component.ts
@@ -1,25 +1,25 @@
-import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
+import { Component, EventEmitter, Input, Output } from '@angular/core';
 
 @Component({
   selector: 'app-custom-button',
   templateUrl: './custom-button.component.html',
   styleUrls: ['./custom-button.component.css']
 })
-export class CustomButtonComponent implements OnInit {
+export class CustomButtonComponent {
 
   @Input()
-  buttonType: string;
+  buttonType: string = 'button'; //default value
 
   @Output()
   click = new EventEmitter<any>();
 
-  constructor() { }
-
-  ngOnInit() {
+  constructor() {
   }
 
-  onClick() {
+  onClick(event: MouseEvent) {
     this.click.emit();
+    //without this line, the click event triggered twice
+    event.stopPropagation();
   }
 
 }
