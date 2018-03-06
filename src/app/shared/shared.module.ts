10d73f591af05b6acb81a60e09cb34739c0b7d83
Beni312 <bence312@gmail.com>
2018-03-06 13:19:58 +0100

    form error display

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -5,12 +5,14 @@ import { CustomInputComponent } from './custom-form/custom.input.component';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
 import { NgModule } from '@angular/core';
+import { ErrorDisplayComponent } from './error-display/error-display.component';
 
 const declareExport = [
   AutocompleteMultiSelectComponent,
   ClickOutsideDirective,
   CustomInputComponent,
-  CustomButtonComponent
+  CustomButtonComponent,
+  ErrorDisplayComponent
 ];
 
 @NgModule({
